b657b41ccde68952d33d9f73fcf46a69a0f7ad1c
yichen <ycc21@ic.ac.uk>
2023-03-02 20:43:46 +0000

    Added further print debugging

--- HtmlTestRunner/case.py
+++ HtmlTestRunner/case.py
@@ -1,4 +1,4 @@
-from unittest import TestCase, SkipTest
+from unittest import TestCase, SkipTest, _ShouldStop
 import contextlib
 import sys
 
@@ -6,7 +6,27 @@ class SubTestSkippableCase(TestCase):
 
     @contextlib.contextmanager
     def testPartExecutor(self, test_case, isTest=False):
-        super.testPartExecutor(self, test_case, isTest)
-        if self.skipped[-1][0] == test_case and self.result_supports_subtests:
-            self.errors.append((test_case, None))
+        old_success = self.success
+        self.success = True
+        try:
+            yield
+        except KeyboardInterrupt:
             raise
+        except SkipTest as e:
+            self.success = False
+            self.skipped.append((test_case, str(e)))
+        except:
+            exc_info = sys.exc_info()
+            if self.expecting_failure:
+                self.expectedFailure = exc_info
+            else:
+                self.success = False
+                self.errors.append((test_case, exc_info))
+            # explicitly break a reference cycle:
+            # exc_info -> frame -> exc_info
+            exc_info = None
+        else:
+            if self.result_supports_subtests and self.success:
+                self.errors.append((test_case, None))
+        finally:
+            self.success = self.success and old_success
